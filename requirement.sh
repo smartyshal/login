ecfb0e171569e5a02a67fcd258cabbcbc04e3716
Shashank Arora <shashank.arora@gmail.com>
2017-08-09 19:31:36 -0700

    login script requirement.sh

---
+++ requirement.sh
@@ -0,0 +1,13 @@
+PYENV_HOME=$WORKSPACE/.pyenv/
+
+# Delete previously built virtualenv
+if [ -d $PYENV_HOME ]; then
+    rm -rf $PYENV_HOME
+fi
+
+# Create virtualenv and install necessary packages
+virtualenv --no-site-packages $PYENV_HOME
+. $PYENV_HOME/bin/activate
+pip install --quiet unittest-xml-reporting
+pip install --quiet selenium==3.4
+pip install --quiet $WORKSPACE/
